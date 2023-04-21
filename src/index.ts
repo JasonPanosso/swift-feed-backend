8b389816bab455a95240e1518f82fd82dfc9ba9f
JasonPanosso <jasonpanosso@protonmail.com>
2023-04-21 13:43:17 -0400

    add eslintrc + prettierrc, fix nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
-const app = express();
 const port = process.env.PORT || 3000;
+const MONGODB_URI = 'mongodb://localhost:27017/swift-feed';
 
-const MONGODB_URI = 'mongodb://localhost:27017/my-app';
 
+const app = express();
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
@@ -24,9 +24,6 @@ mongoose
   )
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
   })
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error.message);
