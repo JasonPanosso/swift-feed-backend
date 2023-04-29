bf47356f4d4b20b8d42802121e3e6ecd81221d29
JasonPanosso <jasonpanosso@protonmail.com>
2023-04-29 10:24:01 -0400

    cleanup ftpservice.ts

--- src/services/FtpService.ts
+++ src/services/FtpService.ts
@@ -84,24 +84,27 @@ ftpServer.on('client:connected', (connection: ftpd.FtpConnection) => {
       event: string,
       data: { user: string; file: string; bytesWritten: number }
     ) => {
+      const filePath = ftpConfig.ftpDir + data.file
       if (data.bytesWritten > ftpConfig.sizeLimit) {
         console.error(
           `Error during file upload: File exceeded max file size of ${ftpConfig.sizeLimit}`
         );
-        fs.rmSync(ftpConfig.ftpDir + data.file);
+        fs.rmSync(filePath);
         return;
       }
       if (event === 'close') {
-        const fileStream = fs.createReadStream(ftpConfig.ftpDir + data.file);
+        const fileStream = fs.createReadStream(filePath);
         const feedProcessingMediator = new FeedProcessingMediator();
         await feedProcessingMediator.processDataFeed(fileStream, data.user);
-        console.log(
-          'file exist status: ' + fs.existsSync(ftpConfig.ftpDir + data.file)
-        );
-        fs.rmSync(ftpConfig.ftpDir + data.file);
-        console.log(
-          'file exist status: ' + fs.existsSync(ftpConfig.ftpDir + data.file)
-        );
+        console.log('Beginning file cleanup');
+        fs.rmSync(filePath);
+        if (fs.existsSync(filePath)) {
+          console.error(`File cleanup failed for ${filePath}`)
+          return
+        } else {
+          console.log(`File cleanup performed successfully for ${filePath}`)
+          return
+        }
       }
     }
   );
